f34629604f1aaf327f79746125c7ed0df2c5e016
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-05-16 02:31:22 -0400

    added css/images mostly

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -17,19 +17,36 @@ document.onkeyup = function(event) {
     //TIE
     alert(`It's a tie!`);
     document.getElementById("tieHTML").innerHTML = ++tie;
+    document.getElementById("tie").setAttribute(
+        "style", 'animation: point-added; transition: 1.5s;');
 
     } else if (userGuess=='r' && computerGuess=='s' || userGuess=='p' && computerGuess=='r' || userGuess=='s' && computerGuess=='p'){
     //USER WINS
     alert('You WIN!');
     document.getElementById("userWins").innerHTML = ++userWin;
+    document.getElementById("userWins").setAttribute(
+        "style", 'animation-name: point-added; transition: 1.5s;');
 
     } else if (computerGuess=='r' && userGuess=='s' || computerGuess=='p' && userGuess=='r' || computerGuess=='s' && userGuess=='p'){
     //COMPUTER WINS 
     alert('Computer wins :(');
     document.getElementById("computerWins").innerHTML = ++computerWin;
+    document.getElementById("computerWins").setAttribute(
+        "style", 'animation: point-added; transition: 1.5s;');
 
     } else {
     //WRONG KEY
     alert('Please choose a valid choice.');
+    }
+
+    //PICS
+    if (userGuess=='r') {
+        document.getElementById('userPic').setAttribute('src','assets/images/rock-edit.png');
+    } else if (userGuess=='s') {
+        document.getElementById('userPic').setAttribute('src','assets/images/scissors-edit.png');
+    } else if (userGuess=='p') {
+        document.getElementById('userPic').setAttribute('src','assets/images/paper-edit.png');
+    } else {
+        document.getElementById('userPic').setAttribute('src','assets/images/shrug-edit.png');
     }
 };
