99014d9aab970c4961f2ddb89953a2d8b893da4e
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-05-16 03:06:41 -0400

    better game

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -10,26 +10,26 @@ document.onkeyup = function(event) {
     var userGuess = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    alert("User guess: " + userGuess);
-    alert("Computer guess: " + computerGuess);
+    // alert("User guess: " + userGuess);
+    // alert("Computer guess: " + computerGuess);
     
     if (userGuess=='r' && computerGuess=='r' || userGuess=='s' && computerGuess =='s' || userGuess=='p' && computerGuess=='p'){
     //TIE
-    alert(`It's a tie!`);
+    // alert(`It's a tie!`);
     document.getElementById("tieHTML").innerHTML = ++tie;
-    document.getElementById("tie").setAttribute(
-        "style", 'animation: point-added; transition: 1.5s;');
+    // document.getElementById("tie").setAttribute(
+    //     "style", 'animation: point-added; transition: 1.5s;');
 
     } else if (userGuess=='r' && computerGuess=='s' || userGuess=='p' && computerGuess=='r' || userGuess=='s' && computerGuess=='p'){
     //USER WINS
-    alert('You WIN!');
+    // alert('You WIN!');
     document.getElementById("userWins").innerHTML = ++userWin;
     document.getElementById("userWins").setAttribute(
         "style", 'animation-name: point-added; transition: 1.5s;');
 
     } else if (computerGuess=='r' && userGuess=='s' || computerGuess=='p' && userGuess=='r' || computerGuess=='s' && userGuess=='p'){
     //COMPUTER WINS 
-    alert('Computer wins :(');
+    // alert('Computer wins :(');
     document.getElementById("computerWins").innerHTML = ++computerWin;
     document.getElementById("computerWins").setAttribute(
         "style", 'animation: point-added; transition: 1.5s;');
@@ -48,5 +48,16 @@ document.onkeyup = function(event) {
         document.getElementById('userPic').setAttribute('src','assets/images/paper-edit.png');
     } else {
         document.getElementById('userPic').setAttribute('src','assets/images/shrug-edit.png');
+    }
+
+    //PICS
+    if (computerGuess=='r') {
+        document.getElementById('compPic').setAttribute('src','assets/images/rock-c.png');
+    } else if (computerGuess=='s') {
+        document.getElementById('compPic').setAttribute('src','assets/images/scissors-c.png');
+    } else if (computerGuess=='p') {
+        document.getElementById('compPic').setAttribute('src','assets/images/paper-c.png');
+    } else {
+        console.log('error');
     }
 };
